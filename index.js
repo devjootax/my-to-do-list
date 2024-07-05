ab80e47673ce2217fe46f5216dabdfef9f54b98d
João Pedro <joao.silva2601@outlook.com.br>
2024-07-04 21:57:33 -0300

    create label function and create li function added

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 let ids = 0;
 
+function createCheckBox() {
+  const input = document.createElement("input");
+  input.id = `radio-${ids}`;
+  input.type = "radio";
+
+  return label;
+}
+
+function createLabel() {
+  const label = document.createElement("label");
+  label.innerText = newTaskInput.value;
+  label.setAttribute("for", `radio-${ids}`);
+
+  return label;
+}
+
+function createLi() {
+  const li = document.createElement("li");
+  li.classList.add("task");
+  li.id = `task-${ids}`;
+}
+
 function createTask() {
   console.log("opa, fui chamada");
   const newTaskInput = document.getElementById("new-task-input");
@@ -7,17 +29,6 @@ function createTask() {
 
   if (newTaskInput.value !== "") {
     ids++;
-    const li = document.createElement("li");
-    li.classList.add("task");
-    li.id = `task-${ids}`;
-
-    const input = document.createElement("input");
-    input.id = `radio-${ids}`;
-    input.type = "radio";
-
-    const label = document.createElement("label");
-    label.innerText = newTaskInput.value;
-    label.setAttribute("for", `radio-${ids}`);
 
     li.append(input, label);
     tasksContainer.appendChild(li);
