608d9931ff44d0f23c9f5b96bef74358fe0606b8
João Pedro <joao.silva2601@outlook.com.br>
2024-07-10 12:08:15 -0300

    saving tasks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const newTaskInput = document.getElementById("new-task-input");
-const tasks = document.getElementById("tasks");
+const tasksContainer = document.getElementById("tasks");
+const tasks = [];
 let ids = 0;
 
 function createLi() {
@@ -25,13 +26,13 @@ function createLabel() {
 
 function createTrashIcon() {
   const img = document.createElement("img");
-  img.id = `trash-icon-${ids}`;
   img.setAttribute("src", "./assets/lixeira.png");
+  img.classList.add("trash-icon");
   return img;
 }
 
 function createTask() {
-  if (newTaskInput.value !== "") {
+  if (newTaskInput.value.trim() !== "") {
     ids++;
 
     const li = createLi();
@@ -39,8 +40,15 @@ function createTask() {
     const label = createLabel();
     const trashIcon = createTrashIcon();
 
+    const newTask = {
+      text: newTaskInput.value,
+      state: "pendente",
+    };
+
+    tasks.push(newTask);
+
     li.append(input, label, trashIcon);
-    tasks.appendChild(li);
+    tasksContainer.appendChild(li);
   } else {
     throw new Error("opa irmao, preenche os campos aí.");
   }
@@ -52,5 +60,10 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   createTask();
 
+  const lis = document.querySelectorAll("li");
+
+  console.log(lis);
+
+  console.log(tasks);
   form.reset();
 });
